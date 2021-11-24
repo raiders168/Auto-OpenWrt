7dd7cac24035fd4b52aadfcc84c2a779c1c6d155
raiders168 <59355325+raiders168@users.noreply.github.com>
2021-11-24 09:01:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,4 +24,4 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Ma
 
 #######安装smartdns（必选）########
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-git clone https://github.com/pymumu/smartdns.git package/smartdns
+#git clone https://github.com/pymumu/smartdns.git package/smartdns
