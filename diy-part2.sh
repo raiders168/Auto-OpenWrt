97304ca97e6a4055a218a095bf73daf88e422943
raiders168 <59355325+raiders168@users.noreply.github.com>
2021-11-24 16:40:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,10 +13,10 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-######更改大雕源码（可选）###########
+########### 更改大雕源码（可选）###########
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 
-######更改默认主题（可选）###########
+########### 更改默认主题（可选）###########
 # 删除自定义源默认的 argon 主题
 rm -rf package/lean/luci-theme-argon
 # 拉取 argon 原作者的源码
@@ -25,6 +25,9 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 #make menuconfig时记得勾选LuCI ---> Applications ---> luci-app-argon-config
 
-#######安装smartdns（必选）########
+########### 安装smartdns（必选）###########
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 #git clone https://github.com/pymumu/smartdns.git package/smartdns
+
+########### 设置密码为空（可选） ###########
+sed-i's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/files/zzz-default-settings
