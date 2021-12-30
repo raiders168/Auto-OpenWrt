534f5abc2d5ef83717285ec86776d6581cdbe58f
raiders168 <59355325+raiders168@users.noreply.github.com>
2021-12-30 20:02:57 +0800

    Update diy-5.4-part2.sh

--- diy-5.4-part2.sh
+++ diy-5.4-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 ########### 更改大雕源码（可选）###########
-#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
 
 ########### 更改默认主题（可选）###########
 # 删除自定义源默认的 argon 主题
