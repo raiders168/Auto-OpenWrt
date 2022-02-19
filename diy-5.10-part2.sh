b017dde38517ba231d86e192059cb8bae22e5d6c
raiders168 <59355325+raiders168@users.noreply.github.com>
2022-02-19 22:38:11 +0800

    Update diy-5.10-part2.sh

--- diy-5.10-part2.sh
+++ diy-5.10-part2.sh
@@ -22,13 +22,9 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 
 ########### 更改默认主题（可选）###########
 # 删除自定义源默认的 argon 主题
-# rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
 # 拉取 argon 原作者的源码
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-cd openwrt
-cd package/lean  
-rm -rf luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 #make menuconfig时记得勾选LuCI ---> Applications ---> luci-app-argon-config
