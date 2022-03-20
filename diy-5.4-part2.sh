3f2d293d9be1a0de539daf446808895d722af0d4
raiders168 <59355325+raiders168@users.noreply.github.com>
2022-03-20 22:36:04 +0800

    Update diy-5.4-part2.sh

--- diy-5.4-part2.sh
+++ diy-5.4-part2.sh
@@ -31,5 +31,5 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefil
 
 ########### 安装smartdns（必选）###########
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-#git clone https://github.com/pymumu/smartdns.git package/smartdns
+# git clone https://github.com/pymumu/smartdns.git package/smartdns
 
