f55dbf5716d2874383196cee1a32be39eacb8dcc
raiders168 <59355325+raiders168@users.noreply.github.com>
2022-03-21 18:23:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,9 @@
 
 
 ####### 科学上网插件 #######
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+
 
 ####### darkmatter主题 #######
 # echo 'src-git darkmatter git://github.com/apollo-ng/luci-theme-darkmatter.git' >>feeds.conf.default
