d2b59286d36253f9c96e7516cc84e5a2548dc54c
raiders168 <59355325+raiders168@users.noreply.github.com>
2022-08-23 10:35:02 +0800

    Update diy-5.19-part2.sh

--- diy-5.19-part2.sh
+++ diy-5.19-part2.sh
@@ -30,7 +30,7 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.19/g' target/linux/x86/Makefi
 # make menuconfig时记得勾选LuCI ---> Applications ---> luci-app-argon-config
 
 ########### 更新lean的内置的smartdns版本 ###########
-sed -i 's/1.2022.37/2022.08.18/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/1.2022.37/1.2022.37.1/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/5a2559f0648198c290bb8839b9f6a0adab8ebcdc/96d3deb595eae4f849cb2a51c69a9be89947fa0d/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
 
