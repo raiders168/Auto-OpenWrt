9b9f7028e4a9158e11a42e83c7029752bd6f8365
raiders168 <59355325+raiders168@users.noreply.github.com>
2022-09-27 17:17:53 +0800

    Update diy-5.19-part2.sh

--- diy-5.19-part2.sh
+++ diy-5.19-part2.sh
@@ -36,6 +36,6 @@ sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
 
 ########### 安装smartdns（必选）###########
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-#git clone https://github.com/pymumu/smartdns.git package/smartdns
+# git clone https://github.com/pymumu/smartdns.git package/smartdns
 
 
